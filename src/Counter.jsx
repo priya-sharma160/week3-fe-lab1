69ea3bcc47579caae940bb1258ef4c03af05994a
AFirose <afrin.techpm@gmail.com>
2026-01-29 15:01:55 +0200

    Step 3-4:

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,20 +1,28 @@
 import './Counter.css';
 
 const Counter = () => {
-    const handleClick = () => {
-    console.log('Button clicked');
+  let theme = 'light';
+
+  const setDarkTheme = () => {
+    theme = 'dark';
+    console.log(theme);
   };
 
-return (
-   <div className="state">
+  const setLightTheme = () => {
+    theme = 'light';
+    console.log(theme);
+  };
+
+  return (
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
-      <button onClick={handleClick}>Dark</button>
-      <button onClick={handleClick}>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>Increment</button>
-      <button>Decrement</button>
-   </div>
-);
+      <button onClick={setDarkTheme}>Dark</button>
+      <button onClick={setLightTheme}>Light</button>
+      <p>
+        Current theme (logged only): <strong>{theme}</strong>
+      </p>
+    </div>
+  );
 };
 
 export default Counter;
